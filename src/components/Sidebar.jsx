d3713c2f211a9e37c8b3d69827f112c9dfcca505
pranalyadav <yadav.pranal94@gmail.com>
2025-01-17 15:38:22 +0530

    wordlwise project

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import styles from "./Sidebar.module.css";
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import { Outlet } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <Outlet />
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by Worldwise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
+
+export default function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
+}
